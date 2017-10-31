fa5ff93226967a255f12f71d2df5891574107d4a
Docinho <livia.juliao@ccc.ufcg.edu.br>
2017-10-31 17:40:08 -0300

    calculando similaridade

--- aprendendo-cf.R
+++ aprendendo-cf.R
@@ -1,3 +1,27 @@
 library(dplyr)
 
+setwd(dir = "CF/")
+dados_alemanha <- read.csv(file = "lastfm-matrix-germany.csv")
+head(dados_alemanha[, c(1, 200:220)])
 
+# eliminando a coluna de usuario para fazer um collaborative filtering por item 
+alemanha_ib <- (dados_alemanha[,!names(dados_alemanha) %in% c("user")])
+
+# Criando uma função cosseno para analisar a similaridade
+getCosine <- function(x, y){
+  this.cosine <- (sum(x*y)/(sqrt(sum(y*y))))
+  return(this.cosine)
+}
+
+alemanha_ib_similaridade <- matrix(NA, nrow=ncol(alemanha_ib), ncol = nrow(alemanha_ib), dimnames = list(colnames(alemanha_ib), rownames(alemanha_ib)))
+
+# percorrendo o dataframe para comparar cada uma das musicas
+# percorrebdi as colunas
+for(coluna in 1:ncol(alemanha_ib)){
+# loop pelas colunas para cada coluna
+  for(col in 1:ncol(alemanha_ib)) {
+    alemanha_ib_similaridade[coluna,col] <- getCosine(as.matrix(alemanha_ib[coluna]), as.matrix(alemanha_ib[col]))
+  }
+}
+
+dados_alemanha_vizinhos <- matrix(NA, nrow = ncol(alemanha_ib_similaridade), ncol = 11, dimnames = list(colnames(alemanha_ib_similaridade)))
